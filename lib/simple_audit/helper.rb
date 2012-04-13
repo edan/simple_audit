b731ee17f447fda73ecdb76425367bb8baf41c43
edan <erosenberg@slingmedia.com>
2012-04-12 21:12:56 -0400

    style change

--- lib/simple_audit/helper.rb
+++ lib/simple_audit/helper.rb
@@ -10,8 +10,8 @@ module SimpleAudit #:nodoc:
       audits.each_with_index do |audit, index|
         older_audit = audits[index + 1]
         res += content_tag(:div, :class => 'audit') do
-          content_tag(:div, audit.action, :class => "action #{audit.action}") +
           content_tag(:div, audit.username, :class => "user") + 
+          content_tag(:div, audit.action, :class => "action #{audit.action}") +
           content_tag(:div, l(audit.created_at), :class => "timestamp") + 
           content_tag(:div, :class => 'changes') do
             changes = if older_audit.present?
